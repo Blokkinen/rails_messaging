ea38b4cb59a21b5e3531d906ca52a0c28091a0a8
blokkinen <viktoria_blok@hotmail.com>
2016-09-09 13:59:47 +0200

    removed unnecessary step

--- features/step_definitions/logout_steps.rb
+++ features/step_definitions/logout_steps.rb
@@ -1,9 +1,3 @@
-Given(/^the following user exist:$/) do |table|
-  table.hashes.each do |hash|
-    FactoryGirl.create(:user, hash)
-  end
-end
-
 Given(/^I am logged in as "([^"]*)"$/) do |name|
   user = User.find_by(name: name)
   login_as(user, scope: :user)
